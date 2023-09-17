ee035054e91c3ac493830d71c6e4c3509c0e1e30
Dimas Alfiansyah <alfi.dim1612@gmail.com>
2023-09-17 18:39:57 +0700

    Refactor: Added Nav bar responsive anchor

--- components/NavBarElement.js
+++ components/NavBarElement.js
@@ -1,44 +1,12 @@
-import { Fragment, useState } from 'react';
+import { useState } from 'react';
 import {
-  Dialog, Disclosure, Popover, Transition,
+  Dialog, Popover,
 } from '@headlessui/react';
 import {
-  ArrowPathIcon,
   Bars3Icon,
-  ChartPieIcon,
-  CursorArrowRaysIcon,
-  FingerPrintIcon,
-  SquaresPlusIcon,
   XMarkIcon,
 } from '@heroicons/react/24/outline';
-import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid';
 import Image from 'next/image';
-
-const products = [
-  {
-    name: 'Analytics', description: 'Get a better understanding of your traffic', href: '#', icon: ChartPieIcon,
-  },
-  {
-    name: 'Engagement', description: 'Speak directly to your customers', href: '#', icon: CursorArrowRaysIcon,
-  },
-  {
-    name: 'Security', description: 'Your customers’ data will be safe and secure', href: '#', icon: FingerPrintIcon,
-  },
-  {
-    name: 'Integrations', description: 'Connect with third-party tools', href: '#', icon: SquaresPlusIcon,
-  },
-  {
-    name: 'Automations', description: 'Build strategic funnels that will convert', href: '#', icon: ArrowPathIcon,
-  },
-];
-const callsToAction = [
-  { name: 'Watch demo', href: '#', icon: PlayCircleIcon },
-  { name: 'Contact sales', href: '#', icon: PhoneIcon },
-];
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ');
-}
 
 export default function NavBarElement() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -66,8 +34,8 @@ export default function NavBarElement() {
           <a href="/#about" className="text-sm font-semibold leading-6 text-gray-900">
             About
           </a>
-          <a href="/#feature" className="text-sm font-semibold leading-6 text-gray-900">
-            Feature
+          <a href="/#features" className="text-sm font-semibold leading-6 text-gray-900">
+            Features
           </a>
           <a href="/#techstack" className="text-sm font-semibold leading-6 text-gray-900">
             Tech stack
@@ -106,48 +74,33 @@ export default function NavBarElement() {
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
-                <Disclosure as="div" className="-mx-3">
-                  {({ open }) => (
-                    <>
-                      <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-                        Product
-                        <ChevronDownIcon
-                          className={classNames(open ? 'rotate-180' : '', 'h-5 w-5 flex-none')}
-                          aria-hidden="true"
-                        />
-                      </Disclosure.Button>
-                      <Disclosure.Panel className="mt-2 space-y-2">
-                        {[...products, ...callsToAction].map((item) => (
-                          <Disclosure.Button
-                            key={item.name}
-                            as="a"
-                            href={item.href}
-                            className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                          >
-                            {item.name}
-                          </Disclosure.Button>
-                        ))}
-                      </Disclosure.Panel>
-                    </>
-                  )}
-                </Disclosure>
                 <a
-                  href="#"
+                  href="/#about"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  onClick={() => setMobileMenuOpen(false)}
+                >
+                  About
+                </a>
+                <a
+                  href="/#features"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  onClick={() => setMobileMenuOpen(false)}
                 >
                   Features
                 </a>
                 <a
-                  href="#"
+                  href="/#techstack"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  onClick={() => setMobileMenuOpen(false)}
                 >
-                  Marketplace
+                  Tech stack
                 </a>
                 <a
-                  href="#"
+                  href="/#contact"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  onClick={() => setMobileMenuOpen(false)}
                 >
-                  Company
+                  Contact
                 </a>
               </div>
               <div className="py-6">
